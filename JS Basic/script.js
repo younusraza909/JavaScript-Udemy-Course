5715b05114329f576803deac2328ae2e97fb30d2
younusraza909 <younusraza909@gmail.com>
2020-02-26 15:04:15 +0500

    JS Basic Part 1 With Code Challenge One

--- JS Basic/script.js
+++
@@ -1,1 +0,0 @@
-console.log("External Source For Js")
---
+++ JS Basic/scriptP1.js
@@ -0,0 +1,131 @@
+/*********************
+ * Variables And DataTypes
+ */
+/*
+var FirstName = "John";
+console.log(FirstName);
+
+var lastName = "Smith";
+var age = 28;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;//undefined is saved in it automatically
+job="teacher"//assigned value to variable
+*/
+
+/*********************
+ * Variables Mutation And type coerction
+ */
+
+/*var FirstName="John";
+var age=28;
+
+//Type Corection
+console.log(FirstName +" "+ age)
+
+var job,isMarried;
+
+job="Teacher";
+isMarried=false;  
+
+console.log(FirstName +" is a "+age +" year old "+job +'.Is he Married? '+isMarried);
+
+//Variable Mutation
+
+age="Twenty Eight";
+job="Driver"
+
+alert(job+" "+age)
+
+var lastName=prompt("What is his last Name ");
+console.log(firstName+" "+lastName)
+*/
+
+/*********************
+ * Basic Operators
+ */
+/*
+//Math Operator 
+var year,yearJohn,yearMark;
+currentYear=2018
+yearJohn=currentYear-28;
+yearMark=currentYear-33
+
+console.log(yearJohn);
+
+console.log(currentYear+2)
+console.log(currentYear*2)
+console.log(currentYear/10)
+
+
+//Logical Operator
+var ageJohn,ageMark;
+ageJohn=28;
+ageMark=33;
+
+consol.log(ageJohn>ageMark)
+
+//typeof Operator
+console.log(typeof ageJohn);
+console.log(typeof ageMark);
+console.log(typeof "Mark is older than john")
+var x;
+console.log(typeof x)
+*/
+
+/*********************
+ * Operators Precedence
+ */
+/*
+ var now=2018;
+ var yearJohn=1989;
+ var fullAge=18;
+
+ //Multiple operator
+ var isFullAge=now-yearJohn>=fullAge;
+ console.log(isFullAge);
+
+ //Grouping
+var ageJohn = now-yearJohn;
+var ageMark =35;
+var avgAge=(ageJohn+ageMark)/2;
+console.log(avgAge);
+
+//Multiple Assignments
+
+var x,y;
+x=y=(3+5)*4-6;//8*4-6//32-6//26
+console.log(x,y)
+
+//2+4+5 //left To Right
+
+//Assignment operator work right to left
+
+
+//More Operator
+x*=2;
+console.log(x);
+x+=10;
+console.log(x)
+
+x++//increament 
+x--//Decreament
+*/
+
+/**********************************
+ * CODING CHALLENGE 1
+ */
+/*var heightJohn= 1.64;
+var heightMark=1.67;
+var weightJohn=90;
+var weightMark=80;
+//Calculationg BMI(Body Mass Index)
+var JohnBMI=weightJohn/(heightJohn*heightJohn)
+var MarkBMI=weightMark/(heightMark*heightMark)
+//Comparing BMI
+var compare=JohnBMI>MarkBMI
+//consoling Result
+console.log("Is John BMI Higher Than Mark BMI",compare)
+*/
