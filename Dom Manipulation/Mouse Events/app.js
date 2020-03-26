13afb3e11e0ba1d71b160cd9a168da689d4dc466
younusraza909 <younusraza909@gmail.com>
2020-03-26 08:28:26 -0700

    Event Listner

---
+++ Dom Manipulation/Mouse Events/app.js
@@ -0,0 +1,29 @@
+const clearBtn = document.querySelector(".clear-tasks");
+const card = document.querySelector(".card");
+const heading = document.querySelector("h5");
+
+// Click
+// clearBtn.addEventListener('click', runEvent);
+// Doubleclick
+// clearBtn.addEventListener('dblclick', runEvent);
+// Mousedown
+// clearBtn.addEventListener('mousedown', runEvent);
+// Mouseup
+// clearBtn.addEventListener('mouseup', runEvent);
+// Mouseenter
+// card.addEventListener('mouseenter', runEvent);
+// Mouseleave
+// card.addEventListener('mouseleave', runEvent);
+// Mouseover
+// card.addEventListener('mouseover', runEvent);
+// Mouseout
+// card.addEventListener('mouseout', runEvent);
+// Mousemove
+card.addEventListener("mousemove", runEvent);
+
+// Event Handler
+function runEvent(e) {
+  heading.textContent = `MouseX:${e.offsetX} MouseY:${e.offsetY}`;
+
+  document.body.style.backgroundColor = `rgb(${e.offsetX},${e.offsetY},50)`;
+}
