b229dfe075db58bf195627c3362de6dc4b687b10
younusraza909 <younusraza909@gmail.com>
2020-03-31 22:19:30 -0700

    Book List OOP Project"

--- Object Oriented Programming/Prototype inheritence/app.js
+++ Object Oriented Programming/Prototype inheritence/app.js
@@ -5,18 +5,17 @@ function Person(firstName, lastName) {
 }
 
 // Greeting
-Person.prototype.greeting = function(){
+Person.prototype.greeting = function() {
   return `Hello there ${this.firstName} ${this.lastName}`;
-}
+};
 
-const person1 = new Person('John', 'Doe');
+const person1 = new Person("John", "Doe");
 
 console.log(person1.greeting());
 
 // Customer constructor
 function Customer(firstName, lastName, phone, membership) {
-  Person.call(this, firstName, lastName);
-
+  Person.call(this);
   this.phone = phone;
   this.membership = membership;
 }
@@ -28,13 +27,13 @@ Customer.prototype = Object.create(Person.prototype);
 Customer.prototype.constructor = Customer;
 
 // Create customer
-const customer1 = new Customer('Tom', 'Smith', '555-555-5555', 'Standard');
+const customer1 = new Customer("Tom", "Smith", "555-555-5555", "Standard");
 
 console.log(customer1);
 
 // Customer greeting
-Customer.prototype.greeting = function(){
+Customer.prototype.greeting = function() {
   return `Hello there ${this.firstName} ${this.lastName} welcome to our company`;
-}
+};
 
 console.log(customer1.greeting());
